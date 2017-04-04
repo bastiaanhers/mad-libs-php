dbdb84fec1ef08e7a5027c5c20ce539a673f4e23
Bastiaan Hersman <bastiaan.hers@gmail.com>
2017-04-04 11:40:12 +0200

    no message

---
+++ er_heerst_paniek.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>mad lips - er heerst paniek</title>
+</head>
+<body>
+<p class="vrag">	Er heerst paniek in het koningkrijk <?php echo $_POST["word3"]; ?>. Koning 																				<?php echo $_POST["word6"]; ?> is ten einde raad en als koning 														<?php echo $_POST["word6"]; ?> ten einde 					 raad is, dan roept hij zijn ten-einde-raadsheer 							<?php echo $_POST["word2"]; ?>. <br><br>
+					"<?php echo $_POST["word2"]; ?>! Het is een ramp! Het is een schande!"<br><br>
+					"Mijn <?php echo $_POST["word1"]; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["word5"]; ?> voor hem gekocht!"<br><br>
+					"Majesteit, uw <?php echo $_POST["word1"]; ?> komt vast vanzelf wel weer terug?"<br><br>
+					"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST["word8"]; ?> leren?"<br><br>
+					"maar Sire, daar kunt u toch uw <?php echo $_POST["word7"]; ?> voor gebruiken."<br><br>
+					"<?php echo $_POST["word2"]; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."<br><br>
+					"<?php echo $_POST["word4"]; ?>, Sire."
+</p>
+</body>
+</html> 
---
+++ onkunde.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>mad-libs - onkunde</title>
+	</head>
+	<body>
+		<main>
+		<h1>onkunde</h1>
+		<div id="main">
+		<nav>
+			<a href="mad-lips.php" class="button">er heerst paniek</a>
+			<a href="" class="button">onkunde</a>
+		</nav>
+		<div id="banaan"><br>
+			<h2>Er heerst paniek...</h2>
+			<div id="text">
+				<p>Welk dier zou je nooit als huisdier willen hebben?</p>
+				<p>Wie is de belangrijkste persoon in je leven?</p>
+				<p>In welk land zou je graag willen wonen?</p>
+				<p>Wat doe je als je je verveelt?</p>
+				<p>Met welk speelgoed speelde je als kind het meest?</p>
+				<p>Bij welk docent spijbel je het liefst?</p>
+				<p>Als je $100.000,- had, wat zou je dan kopen?</p>
+				<p>Wat is je favoriete bezigheid?</p>
+			</div>
+			<div id="input">
+			<form action="er_heerst_paniek.php" method="POST">
+				<input type="text" name="word1" value="">
+				<input type="text" name="word2" value="">
+				<input type="text" name="word3" value="">
+				<input type="text" name="word4" value="">
+				<input type="text" name="word5" value="">
+				<input type="text" name="word6" value="">
+				<input type="text" name="word7" value="">
+				<input type="text" name="word8" value="">
+
+				<input type="submit" name="submit" value="Versturen">
+			</form> 
+			</div>
+
+		</div>
+		</div>
+		<?php
+
+
+
+		?>	
+		</main>
+	</body>
+</html>
